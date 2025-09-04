003943f9b41d8442794a87f7bc6388955eb0764c
imanjeetsodhi <manjeetsinghsodhi06@gmail.com>
2025-09-04 10:46:32 +0530

    list - .split() and .map()

---
+++ week 4 (Lists)/map.py
@@ -0,0 +1,20 @@
+
+# it returns an object, 
+# it has function and itrater, 
+
+# it will take input and convert it into list 
+a = list(map(int, input().split()))
+print(a)
+
+# it will print the object of the element
+b = map(int, input().split())
+print(b)
+
+c = list(map(str, input().split()))
+print(c)
+
+
+
+# to print teh elements of the list
+# for i in (a):
+#     print(i)
---
+++ week 4 (Lists)/split.py
@@ -0,0 +1,15 @@
+
+# use of split function, 
+# it convert a string into list and all the elments in the list is in strin as per the spaces
+
+text = "Hello world, welcome to Python"
+print(text)
+words = text.split()
+print(words)
+
+
+num = "1 2 3 4"
+print(num)
+number = num.split()
+print(number)
+
