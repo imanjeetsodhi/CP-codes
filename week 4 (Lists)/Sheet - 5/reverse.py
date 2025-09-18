1bb9749d647429bc51a55f574aa4ac13d26b5bc5
imanjeetsodhi <manjeetsinghsodhi06@gmail.com>
2025-09-18 09:53:17 +0530

    sheet 5

---
+++ week 4 (Lists)/Sheet - 5/reverse.py
@@ -0,0 +1,10 @@
+a=list(map(int,input().split()))
+for i in range(len(a)):
+    a[i],a[len(a)-i-1]=a[len(a)-i-1],a[i]
+print(a)
+
+
+# method 2
+mylist = [1.4, 2, 3, 4, 5, "Suyash"]
+mylist.reverse()
+print(mylist)
