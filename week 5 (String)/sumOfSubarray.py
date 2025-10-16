d6b6592786e071686806ff56393aa4cb92bcae9c
imanjeetsodhi <manjeetsinghsodhi06@gmail.com>
2025-10-16 10:37:54 +0530

    sumOfSubarray

---
+++ week 5 (String)/sumOfSubarray.py
@@ -0,0 +1,19 @@
+#WAP to print sum of every singlesubarray in an array
+arr = [10,20,30]
+print("array is: ", arr )
+print("The sum of every subarray is: ")
+n = len(arr)
+for i in range(n):
+    for j in range(i,n):
+        print(sum(arr[i:j+1]))
+
+
+#find the subarray with the largest sum and return its subarray
+print("The largest sum is: ")
+max_sum = 0
+for i in range(n):
+    for j in range(i,n):
+        current_sum = sum(arr[i:j+1])
+        max_sum = max(max_sum,current_sum) 
+
+print(max_sum) 
